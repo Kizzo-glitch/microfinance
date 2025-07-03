955465a5c11e9c2fd15c34636fb3f01424feb1c5
Khotso Serabele <serabelekd@gmail.com>
2025-07-03 20:28:41 +0200

    lender docs update

--- lenders/views.py
+++ lenders/views.py
@@ -189,7 +189,7 @@ def upload_lender_docs(request):
 						document_type=doc_type,
 						file=file
 					)
-			return JsonResponse({'success': 'Documents uploaded successfully!', 'redirect_url': '/lenders/view_lender_documents/'})
+			return JsonResponse({'success': 'Documents uploaded successfully!', 'redirect_url': '/view_lender_documents/'})
 		else:
 			return JsonResponse({'error': form.errors})
 	else:
