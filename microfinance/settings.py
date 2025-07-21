38c938fd97589e41a8826d0e5ea9e16f1fe11bdb
Khotso Serabele <serabelekd@gmail.com>
2025-07-21 05:39:39 +0200

    email reset

--- microfinance/settings.py
+++ microfinance/settings.py
@@ -192,7 +192,6 @@ SMS_API_SECRET = os.getenv('SMS_API_SECRET')
 CLIENT_ID = os.getenv('CLIENT_ID')
 
 
-
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Replace with your email provider's SMTP server
@@ -200,6 +199,6 @@ EMAIL_PORT = 587  # Use the appropriate port
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') 
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')  
 EMAIL_USE_SSL = False
 
