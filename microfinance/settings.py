371036e0e1ef8c35235a9a3c1efde9465e8df494
Kizzo-glitch <serabelekd@gmail.com>
2025-11-12 10:38:02 +0200

    l-details urls

--- microfinance/settings.py
+++ microfinance/settings.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -196,8 +197,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # Replace with your email provider's SMTP server
-EMAIL_PORT = 587  # Use the appropriate port
+EMAIL_HOST = 'smtp.gmail.com' 
+EMAIL_PORT = 587  
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') 
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  
 EMAIL_USE_TLS = True
